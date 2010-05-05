abcb0e2b1cbf30c979bdb192025982069d3d2050
Ben Adida <ben@adida.net>
2010-05-04 18:48:43 -0700

    updated params

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
 
-TEMPLATE_BASE = "auth/templates/base.html"
+from django.conf import settings
+
+TEMPLATE_BASE = settings.AUTH_TEMPLATE_BASE or "auth/templates/base.html"
 
 # enabled auth systems
 import auth_systems
-ENABLED_AUTH_SYSTEMS = auth_systems.AUTH_SYSTEMS.keys()
-DEFAULT_AUTH_SYSTEM = None
+ENABLED_AUTH_SYSTEMS = settings.AUTH_ENABLED_AUTH_SYSTEMS or auth_systems.AUTH_SYSTEMS.keys()
+DEFAULT_AUTH_SYSTEM = settings.AUTH_DEFAULT_AUTH_SYSTEM or None
 
-def get_enabled_auth_systems():
-  pass
-
-def get_default_auth_system():
-  pass
